33fe49b3f812168620e5415aa2056bd7384bf31e
deltadu <jianlind@andrew.cmu.edu>
2019-12-08 01:53:51 -0500

    OpenMP working version done

--- code/src/multiprocessor/openmp2.cpp
+++ code/src/multiprocessor/openmp2.cpp
@@ -8,8 +8,12 @@
 #include "../timing.h"
 using namespace std;
 
-void parallelFloyd(Graph& graph);
+void parallelFloyd(Graph& graph, const int n, const int b);
 void print_result(Graph& graph);
+
+// extern "C" void helper_ispc(Graph& graph, int ar, int ac, int br, int bc, int cr, int cc, const int b, const int n);
+
+#define BLOCK_SIZE 64
 
 int main(int argc, char* argv[]) {
     int numVertices, numberThreads, sqrtOfnumberThreads;
@@ -20,46 +24,61 @@ int main(int argc, char* argv[]) {
     numberThreads = atoi(argv[2]);
     sqrtOfnumberThreads = sqrt(numberThreads);
 
-    if (sqrt(numberThreads) != (double)sqrtOfnumberThreads ||
-        numVertices % sqrtOfnumberThreads != 0) {
-        throw new runtime_error(
-            "Number of vertices must be a multiple of the square root of the "
-            "number of threads");
-    }
-
     omp_set_num_threads(numberThreads);
 
-    Timer t;
-    parallelFloyd(graph);
-    double OpenMPTime = t.elapsed();
-
+    double total_time = 0.0;
+    int experiment_number = 5;
+    for (int i = 0; i < experiment_number; i++) {
+        Timer t;
+        parallelFloyd(graph, numVertices, BLOCK_SIZE);
+        double OpenMPTime = t.elapsed();
+        total_time += OpenMPTime;
+    }
     // cout << "Result is:" << endl;
     // print_result(graph);
-    cout << "Time taken: " << OpenMPTime << endl;
+    cout << "Time taken: " << (total_time / experiment_number) << endl;
     return 0;
 }
 
-void parallelFloyd(Graph& graph) {
-    int lenIKJ;
-    int numVertices = graph.getNumVertices();
 
-    for (int k = 0; k < numVertices; k++) {
-        #pragma omp parallel num_threads(4)
-        for (int i = 0; i < numVertices; i++) {
-            for (int j = 0; j < numVertices; j++) {
-                if (i != j && i != k && j != k) {
-                    lenIKJ =
-                        graph.getDist(i, k) +
-                        graph.getDist(k, j);
-                    graph.setDist(
-                        i, j,
-                        min(lenIKJ, graph.getDist(i, j)));
+inline void helper(Graph& graph, int ar, int ac, int br, int bc, int cr, int cc, const int b, const int n) {
+    for (int k = 0; k < b; k++) {
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < b; j++) {
+                int sum = graph.getDist(i+ar, k+ac) + graph.getDist(k + br, j+bc);
+
+                if (graph.getDist(i+cr, j+cc) > sum) {
+                    graph.setDist(i+cr, j+cc, sum);
                 }
             }
         }
     }
 }
 
+
+void parallelFloyd(Graph& graph, const int n, const int b) {
+    const int blocks = n / b;
+
+    for (int k = 0; k < blocks; k++) {
+        helper(graph, k*b, k*b, k*b, k*b, k*b, k*b, b, n);
+
+        #pragma omp parallel for
+        for (int j = 0; j < blocks; j++) {
+            if (j == k) continue;
+            helper(graph, k*b, j*b, k*b, k*b, k*b, j*b, b, n);
+        }
+
+        #pragma omp parallel for
+        for (int i = 0; i < blocks; i++) {
+            if (i == k) continue;
+            helper(graph, i*b, k*b, i*b, k*b, k*b, k*b, b, n);
+            for (int j = 0; j < blocks; j++) {
+                if (j == k) continue;
+                helper(graph, i*b, j*b, i*b, k*b, k*b, j*b, b, n);
+            }
+        }
+    }
+}
 
 void print_result(Graph& graph) {
     int numVertices = graph.getNumVertices();
